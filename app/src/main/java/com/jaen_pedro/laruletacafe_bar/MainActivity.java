281adeb3ffc86b4a27ebb976a8b3fcd2773432d3
Pedro Jaen <pedro.jaenvilla@gmail.com>
2020-10-28 11:55:52 +0100

    Initial commit

---
+++ app/src/main/java/com/jaen_pedro/laruletacafe_bar/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jaen_pedro.laruletacafe_bar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
